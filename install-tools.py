32c3b29e9b1737b35a9dc7109b0e60e4a47a6eb2
TheZihanGu <i@zihangu.cn>
2020-01-20 08:40:02 +0800

    Update install-tools.py

--- install-tools.py
+++ install-tools.py
@@ -5,16 +5,16 @@ print ("[1] 下载XiaoPE [2] 将XiaoPE写入到U盘 [3]退出")
 choose = input("")
 choose = int(choose)
 if choose == 1:
-    download = input ("请选择一个盘符以供下载临时文件（大写）（如下载到D盘就输入D)：")
+    download = input ("请选择一个盘符以供下载临时文件(大写)：")
     os.system("mkdir " + download + ":\XiaoPE-Download")
     download = download + ":\XiaoPE-Download"
     os.system("aria2c.exe -s 2 -x 2 -j 10 -d " + download + " https://d.xiaope.net/cn_xiaope_9.1_191228_x64.iso")
     os.system("pause")
 if choose == 2:
     print ("警告!接下来将不会有任何提示!安装时将会格式化您的U盘!")
-    drive = input ("请输入将要安装的U盘盘符（大写）（如要安装到F盘就输入F)：")
+    drive = input ("请输入将要安装的U盘盘符(大写)：")
     drive = drive + ":"
-    download = input ("请输入下载时输入的临时文件存储盘符（大写）（如下载到D盘就输入D)：")
+    download = input ("请输入下载时输入的临时文件存储盘符(大写)")
     download = download + ":\XiaoPE-Download\cn_xiaope_9.1_191228_x64.iso"
     os.system("format " + drive +" /q /FS:FAT32 /y")
     os.system("7z.exe x " + download + " -o" + drive)
